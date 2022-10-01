8b4fe8612d8d6f8f1e275be1ff1d77f6e21c0e16
Cruizer <Cruizer@DESKTOP-2AM5BM9>
2022-10-01 14:35:07 +0200

    Oblig 2

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -2,63 +2,144 @@ package no.hvl.dat100.lab5.tabeller;
 
 public class Tabeller {
 
-	// a)
+//	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+	 for(int i=0;i<tabell.length;i++) {
+		  
+	}
 
 	}
 
-	// b)
+//	// b)
 	public static String tilStreng(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+		String x= "";
+		String tmp = "[";
+		int k=0;
+		int sum =0;
+//		System.out.print("[");
+	
+			
 		
+		for(int i=0;i<tabell.length;i++) {
+			x=Integer.toString(tabell[i]);
+			if(i==tabell.length-1) {
+				tmp=tmp+x;
+			} else {
+				tmp=tmp+x+",";
+			}
+
+		}	
+		tmp=tmp+"]";
+		System.out.print(tmp);	
+		return tmp;
 	}
 
-	// c)
-	public static int summer(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
+////	 c)
+	public static int summer(int[] tabell) {  //for
+		int sum =0;
+		int tmp =0;
+		for(int i=0;i<tabell.length;i++) {
+			tmp = tabell[i];
+			sum= tmp+sum;
+			
+		}
+		return sum;
 	}
-
+//	public static int summer(int[] tabell) { //while
+//		int sum =0;
+//		int i =0;
+//		int tmp =0;
+//		while(i<tabell.length) {
+//			sum+= tabell[i];
+//			sum=tmp+sum;
+//			i++;
+//		}
+//		System.out.print(sum);
+//		return sum;
+//	}
+//	public static int summer(int[] tabell) {     // utvidet for
+//		int summ1 =0;
+//		for (int summ : tabell) {
+//			summ1 +=summ;
+//		 
+//	 }
+//		System.out.print(summ1);
+//	return summ1;
+//	
+//}
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
+		int pos =0;
+		boolean funnet= false;
+		
+		while(!funnet &&pos<tabell.length) {
+			if(tall==tabell[pos]) {
+				funnet=true;
+			}
+			pos++;
+		}
+	//	System.out.println(funnet);
+		return funnet;
+		
+		
+		
+	
 	}
-
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
-
+		int index=-1;
+		for(int i=0; i<tabell.length;i++) {
+			if(tabell[i]==tall) {
+				index =i;
+			}
+		}
+		return index;
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("reverser ikke implementert");
-		
+		int [] tabell2 = new int [tabell.length];
+		int p=0;
+	 for(int i=tabell.length-1;i>=0;i--) {
+		 tabell2[p]	= tabell[i];
+					p++;
+				
+			}
+	 for(int i=0;i<tabell.length;i++) {
+		 	System.out.println(tabell2[i]);
+		}
+		 
+		return tabell2;
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
+		boolean sorter =true;
+		for (int i =0;i<tabell.length-1;i++) {
+			if(tabell[i]>tabell[i+1]) {
+				sorter =false;
+			}
+		}
+//		System.out.println(sorter);
+		return sorter;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-
-		// TODO
-		throw new UnsupportedOperationException("settSammen ikke implementert");
+		int pos=0;
+	int [] nytab =new int[tabell1.length+tabell2.length];
+	for (int element : tabell1) {
+		nytab[pos] =element;
+		pos++;
 	}
+	for(int element: tabell2) {
+		nytab[pos]=element;
+		pos++;
+	}
+		
+
+	 return nytab;
+}
 }
--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -5,31 +5,119 @@ public class Matriser {
 	// a)
 	public static void skrivUt(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		int pos=0;
+		for(int tall:matrise[pos]){
+			System.out.println(tall);
+		}
+		pos ++;
+		for(int tall:matrise[pos]){
+			System.out.println(tall);
+		}
+		pos++;
+		for(int tall:matrise[pos]){
+			System.out.println(tall);
+		}
+			
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+		int [] nytab =new int[matrise[0].length+matrise[1].length+matrise[2].length];
+		String []StringArray =new String[nytab.length];
+		int pos=0;
+		int i=0;
+		String tmp="";
+		String test="";
+		String total="";
+		String mellomrom="\r\n";
+		for(int tall:matrise[pos]){
+	//		System.out.println(tall);
+			nytab[i] =tall;
+			test= Integer.toString(tall);
+			StringArray[i] =test;
+			tmp =test;
+			 total= total+tmp+" ";
+			i++;
+		}
+		pos ++;
+		total = total+mellomrom;
+		for(int tall:matrise[pos]){
+	//		System.out.println(tall);
+			nytab[i] =tall;
+			test= Integer.toString(tall);
+			StringArray[i] =test;
+			tmp =test;
+			 total=total+tmp+" ";
+			i++;
+			
+		}
+		pos++;
+		total = total+mellomrom;
+		for(int tall:matrise[pos]){
+	//		System.out.println(tall);
+			nytab[i] =tall;
+			test= Integer.toString(tall);
+			StringArray[i] =test;
+			tmp =test;
+			 total= total+tmp+" ";
+			i++;
+		}
+		total=total+ mellomrom;
+		
+		System.out.println(total);
+		
+		return total;
 		
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
+		int pos =0;
+		int i1=0;
+		int i2=0;
+		int i3=0;
+		int tmp=0;
+		int sum=0;
+		int[][]nymat= new int[3][3];
+		for( int teller:matrise[pos]) {
+			tmp=teller;
+			sum= teller*tall;
+			nymat[pos][i1]=sum;
+			i1++;
+		}
+		pos++;
+		for( int teller:matrise[pos]) {
+			tmp=teller;
+			sum= teller*tall;
+			nymat[pos][i2]=sum;
+			i2++;
+		}
+
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
+		pos++;
+		for( int teller:matrise[pos]) {
+			tmp=teller;
+			sum= teller*tall;
+			nymat[pos][i3]=sum;
+			i3++;
+//			System.out.println(teller);
+		}
+
+		return nymat;
+	
 	
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		boolean erlik = false;
+		if(a==b) {
+			erlik= true;
+		} else {
+			erlik= false;
+		}
+		return erlik;
+	
 	}
 	
 	// e)
